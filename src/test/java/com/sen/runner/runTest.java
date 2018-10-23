b76da359c51317673ba8bfc3ce5ac3c75cc81cfe
aksenthilk <aksenthilk@gmail.com>
2018-10-23 11:08:24 +0530

    Changed all reports path

--- src/test/java/com/sen/runner/runTest.java
+++ src/test/java/com/sen/runner/runTest.java
@@ -23,10 +23,15 @@ import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;*/
                          outputFolder = "target")*/
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "G:\\SeleniumCucumberWorkSpace\\OctoberCucumber\\src\\main\\java\\com\\sen\\feature", glue = {
+/*@CucumberOptions(features = "G:\\SeleniumCucumberWorkSpace\\OctoberCucumber\\src\\main\\java\\com\\sen\\feature", glue = {
 		"com\\sen\\stepDefinition" }, strict = true, dryRun = false, plugin = { "html:com\\sen\\reports\\result",
 				"rerun:target\\rerun.txt", "com.cucumber.listener.ExtentCucumberFormatter:src/test/resource//com/sen/reports/ExtentSen.html" })
-                            
+                     */    
+@CucumberOptions(features = "G:\\SeleniumCucumberWorkSpace\\OctoberCucumber\\src\\main\\java\\com\\sen\\feature", glue = {
+"com\\sen\\stepDefinition" }, strict = true, dryRun = false, plugin = { "html:output\\result.html",
+		"rerun:output\\rerun.txt", "com.cucumber.listener.ExtentCucumberFormatter:output\\ExtentSen.html" })
+             //**/target/myreport/index.html,**/output/report.html,**/target/myreport/embedded0.png,**/target/myreport/embedded1.png
+
 public class runTest {
 
 	// static WebDriver driver;
